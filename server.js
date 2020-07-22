d63724d6432fdd253bf5efabe769b190951c8f6b
hammad-siraj <hammadsiraj224@gmail.com>
2020-07-22 23:24:55 +0500

    addLogger

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+
+  next();
+};
+app.use(logger);
+
 app.use("/api/v1/bootcamps", bootcamp);
 
 app.listen(PORT, console.log(`SEVER IS RUNNING AT PORT${PORT}`));
